42700244688a38507431c2f77aa9d8621bd4e882
Ben Stretar <ben.stretar@gmail.com>
2020-04-26 10:26:41 -0500

    all assets

--- main.py
+++ main.py
@@ -16,3 +16,6 @@ RED_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
 GREEN_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
 BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
 YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
+
+# Background
+BG = pygame.image.load(os.path.join("assets", "background-black.png"))
