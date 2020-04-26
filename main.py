edd16e4fd0c872dcba90455a71eeb8e7a639ac19
Ben Stretar <ben.stretar@gmail.com>
2020-04-26 11:10:17 -0500

    lives and level on screen

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import os
 import time
 import random
+pygame.font.init()
 
 WIDTH, HEIGHT = 750, 750
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -21,5 +22,36 @@ GREEN_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
 BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
 YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
 
-# Background
-BG = pygame.image.load(os.path.join("assets", "background-black.png"))
+# Background (pygame.transform.scale(), (WIDTH, HEIGHT) makes it the window size)
+BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
+
+def main():
+    run = True
+    FPS = 60
+    level = 1
+    lives = 5
+    # adding font to the game
+    main_font = pygame.font.SysFont("comicsans", 50)
+
+    clock = pygame.time.Clock()
+
+    def redraw_window():
+        WIN.blit(BG, (0,0))
+        # draw text
+        lives_lable = main_font.render(f"Lives: {lives}", 1, (255, 255, 255))
+        level_label = main_font.render(f"Level: {level}", 1, (255, 255, 255))
+        
+        WIN.blit(lives_lable, (10, 10))
+        WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
+
+        pygame.display.update()
+
+    while run: 
+        clock.tick(FPS)
+        redraw_window()
+
+        # exit the game when window 'x' is clicked
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+main()
